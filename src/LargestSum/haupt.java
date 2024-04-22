1884100d7142246955d3b2b479b93ccaac2faf5f
Vincent <147661349+Skzter@users.noreply.github.com>
2024-04-22 17:11:09 +0200

    changes

--- src/LargestSum/haupt.java
+++ src/LargestSum/haupt.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 public class haupt {
 
 	public static void main(String[] args) throws Exception {
-		File Datei = new File("/home/vincent/Uni/2.Semester/AOP/seminar/ProjectEulerAufgaben/src/LargestSum/zahl.txt");
-		Scanner input = new Scanner(Datei);
+		//File Datei = new File("/home/vincent/Uni/2.Semester/AOP/seminar/ProjectEulerAufgaben/src/LargestSum/zahl.txt");
+		File DateiW = new File("E:\\Uni\\programmiershit\\eclipse-shit\\eclipse\\ProjectEulerAufgaben\\src\\LargestSum\\zahl.txt");
+		Scanner input = new Scanner(DateiW);
 		
 		int sum = 0;
 		int rest = 0;
 		int uebertrag = 0;
+		short[][] zahlen = new short[100][50];
 		
+		for(int i = 0; i < 100; i++) {
+			String line = input.next();
+			for(int j = 0; j < 50; j++) {
+				zahlen[i][j] = (short)Character.getNumericValue(line.charAt(j));
+			}
+		}
 		/*
 		for(int i = 3; i >= 0; i--) {
 			while(input.hasNextLine()) {
@@ -28,11 +36,8 @@ public class haupt {
 			System.out.println("Rest " + rest);
 			System.out.println("Rest " + uebertrag);
 			*/
-		for(int i = 0; i < 3; i++) {
-			while(input.hasNextLine()) {
-				sum += Character.getNumericValue(input.nextLine().charAt(i));
-			}
-			System.out.println(sum);
+		for(int i = 0; i < 100; i++) {
+			System.out.println(zahlen[i][49]);
 		}
 	}
 }
